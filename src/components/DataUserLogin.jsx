968f1ddc58552769eea83d6e534e6b56b7422fd4
Gabriel <gabrielpbenedicto@gmail.com>
2021-12-08 18:33:46 -0300

    Corrigido requisitos 6 e 7

--- src/components/DataUserLogin.jsx
+++ src/components/DataUserLogin.jsx
@@ -1,36 +1,57 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { actionLogin } from '../redux/actions';
+import Context from '../services/Context';
 
 export default function DataUserLogin() {
-  const [userEmail, setUserEmail] = React.useState('');
-  const [userPassword, setUserPassword] = React.useState('');
+  const { login, changeLogin } = useContext(Context);
+  const { email, password } = login;
   const [buttonActivated, setButtonActivated] = React.useState(false);
+  const saveLogin = useDispatch();
 
   function handleEntrarButton() {
-    const minLengthPassword = 5;
-    const emailIsValid = userEmail.includes('@') && userEmail.includes('.com');
-    const passwordIsValid = userPassword.length > minLengthPassword;
+    const minLengthPassword = 6;
+    const emailIsValid = email.includes('@') && email.includes('.com');
+    const passwordIsValid = password.length > minLengthPassword;
     if (emailIsValid && passwordIsValid) {
+      localStorage.setItem('mealsToken', JSON.stringify(1));
+      localStorage.setItem('cocktailsToken', JSON.stringify(1));
+      localStorage.setItem('user', JSON.stringify({ email }));
       return setButtonActivated(true);
     }
+
     return setButtonActivated(false);
   }
 
+  const onButtomSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem('user', JSON.stringify({ email }));
+    saveLogin(actionLogin(email));
+  };
+
+  const onChangeInput = ({ target }) => {
+    const { name, value } = target;
+    changeLogin({ ...login, [name]: value });
+  };
+
   React.useEffect(() => {
     handleEntrarButton();
-  }, [userEmail, userPassword]);
+  }, [email, password]);
   return (
-    <main>
+    <form onSubmit={ onButtomSubmit }>
       <input
-        value={ userEmail }
-        onChange={ ({ target }) => setUserEmail(target.value) }
+        value={ email }
+        name="email"
+        onChange={ onChangeInput }
         type="text"
         placeholder="Digite seu email"
         data-testid="email-input"
       />
       <input
-        value={ userPassword }
-        onChange={ ({ target }) => setUserPassword(target.value) }
+        value={ password }
+        name="password"
+        onChange={ onChangeInput }
         type="password"
         placeholder="Digite sua senha"
         data-testid="password-input"
@@ -38,12 +59,12 @@ export default function DataUserLogin() {
       <Link to="/comidas">
         <button
           disabled={ !buttonActivated }
-          type="button"
+          type="submit"
           data-testid="login-submit-btn"
         >
           Entrar
         </button>
       </Link>
-    </main>
+    </form>
   );
 }
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,11 +1,3 @@
 export const GET_EMAIL = 'GET_EMAIL';
 
-export const actionLogin = (payload) => {
-<<<<<<< HEAD
-  localStorage.setItem('mealsToken', 1);
-  localStorage.setItem('cocktailsToken', 1);
-=======
->>>>>>> main-group-22
-  localStorage.setItem('user', JSON.stringify(payload));
-  return ({ type: GET_EMAIL, payload });
-};
+export const actionLogin = (payload) => ({ type: GET_EMAIL, payload });
