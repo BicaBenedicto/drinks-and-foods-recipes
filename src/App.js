5d2dfc79797f6cb89513e3136e37300550a5f2a7
Gabriel <gabrielpbenedicto@gmail.com>
2021-12-08 16:22:43 -0300

    Preparação do projeto e componente tela de login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/Login';
+import Context from './services/Context';
+import store from './redux/store/index';
 
 function App() {
+  const [login, changeLogin] = useState({ email: '', password: '' });
+
+  const STORE_CONTEXT = {
+    login,
+    changeLogin,
+  };
+
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Provider store={ store }>
+      <Context.Provider value={ STORE_CONTEXT }>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          {/* <Route path="/comidas" component={ Login } />
+          <Route path="/bebidas" component={ Login } />
+          <Route path='/comidas/:id' component={ Login } /> */}
+        </Switch>
+      </Context.Provider>
+    </Provider>
   );
 }
 
---
+++ src/tests/Login.test.js
@@ -0,0 +1,24 @@
+// // import React from 'react';
+// import { screen } from '@testing-library/react';
+// // import { userEvent } from '@testing-library/user-event';
+// import App from '../App';
+
+// describe('Tela de Login', () => {
+//   test('Verifica se existe um campo de input email', () => {
+// //    const { getById } = render(<App />);
+//     const inputEmail = screen.getById('email-input');
+//     expect(inputEmail).toBeInTheDocument();
+//   });
+
+//   test('Verifica se existe um campo de input password', () => {
+// //     const { getById } = render(<App />);
+//     const inputPassword = screen.getById('password-input');
+//     expect(inputPassword).toBeInTheDocument();
+//   });
+
+//   test('Verifica se existe um botão de sumbit', () => {
+// //     const { getById } = render(<App />);
+//     const buttonSumbit = screen.getById('login-submit-btn"');
+//     expect(buttonSumbit).toBeInTheDocument();
+//   });
+// });
