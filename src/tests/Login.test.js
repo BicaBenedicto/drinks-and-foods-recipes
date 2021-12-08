c4ff87347a9d3e29b1ebd97e98cb6ed2d04fd22a
Gracio <gustavogracio@hotmail.com>
2021-12-08 17:56:47 -0300

    Testes da tela de login

---
+++ src/tests/Login.test.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+
+const EMAIL = 'trybe@trybe.com';
+const INCORRECT_EMAIL = 'trybetrybe';
+const PASSWORD = '123456';
+const INCORRECT_PASSWORD = '12345';
+
+const components = () => ({
+  inputEmail: screen.getByTestId('email-input'),
+  inputPassword: screen.getByTestId('password-input'),
+  buttonSumbit: screen.getByTestId('login-submit-btn'),
+});
+
+describe('Tela de Login', () => {
+  test(`Verfica se o botão esta desativado caso o email for inválido
+  e se a senha deve tiver 6 caracteres ou menos`, () => {
+    renderWithRouter(<App />);
+    const { inputEmail, inputPassword, buttonSumbit } = components();
+    userEvent.type(inputEmail, INCORRECT_EMAIL);
+    userEvent.type(inputPassword, INCORRECT_PASSWORD);
+    expect(buttonSumbit).toBeInTheDocument();
+    expect(buttonSumbit).toBeDisabled();
+  });
+
+  test(`Verfica se o botão deve estar ativado se o email
+   e a senha forem válidos`, () => {
+    renderWithRouter(<App />);
+    const { inputEmail, inputPassword, buttonSumbit } = components();
+    userEvent.type(inputEmail, EMAIL);
+    userEvent.type(inputPassword, PASSWORD);
+    expect(buttonSumbit).toBeInTheDocument();
+    expect(buttonSumbit).not.toBeDisabled();
+  });
+
+  test(`Após a submissão mealsToken e cocktailsToken devem estar salvos em 
+  localStorage`, () => {
+    renderWithRouter(<App />);
+    const { inputEmail, inputPassword, buttonSumbit } = components();
+    userEvent.type(inputEmail, EMAIL);
+    userEvent.type(inputPassword, PASSWORD);
+    userEvent.click(buttonSumbit);
+    const mealsToken = localStorage.getItem('mealsToken');
+    const cocktailsToken = localStorage.getItem('cocktailsToken');
+    expect(mealsToken).toBe(1);
+    expect(cocktailsToken).toBe(1);
+  });
+
+  test(`Verifica se após a submissão a chave user deve estar salva em 
+  localStorage`, () => {
+    renderWithRouter(<App />);
+    const { inputEmail, inputPassword, buttonSumbit } = components();
+    userEvent.type(inputEmail, EMAIL);
+    userEvent.type(inputPassword, PASSWORD);
+    userEvent.click(buttonSumbit);
+    const user = JSON.parse(localStorage.getItem('user'));
+    expect(user).toBe(EMAIL);
+  });
+
+  test('Verifica se rota muda para a tela principal de receitas de comidas', () => {
+    const { history: { location } } = renderWithRouter(<App />);
+    const { inputEmail, inputPassword, buttonSumbit } = components();
+    userEvent.type(inputEmail, EMAIL);
+    userEvent.type(inputPassword, PASSWORD);
+    userEvent.click(buttonSumbit);
+    expect(location.pathname).toBe('/comidas');
+  });
+});
