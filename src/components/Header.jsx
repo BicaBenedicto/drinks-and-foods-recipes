ffc9ccf570d1d98a8e187b19cb0656c9929bc566
Danilo <dcalado.fisio@yahoo.com.br>
2021-12-09 10:28:01 -0300

    add redirect to perfil and search bar click

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import SearchBar from './SearchBar';
+import '../styles/header.css';
 
 function Header(props) {
+  const history = useHistory();
+  const redirectToPerfil = () => history.push('/perfil');
+  const search = useState(true);
+  const [renderSearchBar, setRenderSearchBar] = useState(false);
   const { pageTitle } = props;
 
   return (
-    <header id="header-main">
-      <img
-        src={ profileIcon }
-        alt="profile-icon"
-        className="header-profile-icon"
-        data-testid="profile-top-btn"
-      />
-      <h1
-        className="header-page-title"
-        data-testid="page-title"
-      >
-        {pageTitle}
-      </h1>
-      <button type="button">
-        <img
-          src={ searchIcon }
-          alt="search-icon"
-          className="header-search-icon"
-          data-testid="search-top-btn"
-        />
-      </button>
-    </header>
+    <div>
+      <header className="header-main" id="header-main">
+        <button
+          type="button"
+          data-testid="profile-top-btn"
+          onClick={ redirectToPerfil }
+          src={ profileIcon }
+        >
+          <img
+            src={ profileIcon }
+            alt="profile-icon"
+            className="header-profile-icon"
+            data-testid="profile-top-btn"
+          />
+        </button>
+        <h1 className="header-page-title" data-testid="page-title">
+          { pageTitle }
+        </h1>
+        { search && (
+          <button type="button">
+            <img
+              src={ searchIcon }
+              alt="search-icon"
+              className="header-search-icon"
+              data-testid="search-top-btn"
+              onClick={ () => setRenderSearchBar(!renderSearchBar) }
+            />
+          </button>)}
+      </header>
+      { renderSearchBar && <SearchBar /> }
+    </div>
   );
 }
 
