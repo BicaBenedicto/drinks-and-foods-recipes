824776641b70ed27d03a9b8bf7e23d7052125c36
Gracio <gustavogracio@hotmail.com>
2021-12-10 20:11:35 -0300

    Corrigi erros de lint

--- src/pages/Comidas.js
+++ src/pages/Comidas.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { actionFetchList, actionFetchCategory, actionFetchName } from '../redux/actions';
 import Cards from '../components/Cards';
